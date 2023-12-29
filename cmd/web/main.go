70daf4bae9244c5604351359695c74c088e24b55
FerMusicComposer <lfernandosalamanca@gmail.com>
2023-12-29 22:17:47 +0100

    added fileserver added styles, imgs and JS

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("C:\\Users\\MSI\\Documents\\projects\\go\\lets-go-snippetbox\\ui\\static\\"))
+
+	mux.Handle("C:\\Users\\MSI\\Documents\\projects\\go\\lets-go-snippetbox\\ui\\static\\", http.StripPrefix("\\static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
